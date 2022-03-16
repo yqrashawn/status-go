5af02c19b62e1969b1aeaf2f5aae58fc45e09096
Sale Djenic <aleksandardjenic@stauts.im>
2022-03-16 12:22:12 +0100

    updated cmake script

---
+++ scripts/build_statusgo_static_win.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set ROOT_DIR=%~dp0
+
+cd %ROOT_DIR%
+cd .. 
+
+set GOBIN=%CD%
+
+set ENABLE_METRICS=true
+
+for /F %%F in ('type VERSION') do set RELEASE_TAG=%%F
+for /F %%F in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%F
+
+set BUILD_FLAGS=-ldflags=" -X github.com/status-im/status-go/params.Version=%RELEASE_TAG% -X github.com/status-im/status-go/params.GitCommit=%GIT_COMMIT% -X github.com/status-im/status-go/vendor/github.com/ethereum/go-ethereum/metrics.EnabledStr=%ENABLE_METRICS%"
+
+md "%GOBIN%\build\bin\statusgo-lib" 2>nul
+
+echo "create `main.go`..."
+go run %GOBIN%\cmd\library 2 > %GOBIN%\build\bin\statusgo-lib\main.go
+
+echo "building status-go static lib..."
+go build %BUILD_FLAGS% -buildmode=c-archive -o %GOBIN%\build\bin\libstatus.lib %GOBIN%\build\bin\statusgo-lib
+echo "status-go static lib built..."
